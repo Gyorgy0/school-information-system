a5604a0aad8b4a49af1e05f30e1fd9898a7deb58
topertz <k.markuci@gmail.com>
2025-06-01 07:58:10 +0200

    Delete Controllers/LunchItem.cs

--- Controllers/LunchItem.cs
+++
@@ -1,9 +0,0 @@
-namespace SchoolAPI.Models
-{
-    public class LunchItem
-    {
-        public int LunchID { get; set; }
-        public string? Day { get; set; }
-        public string? Meal { get; set; }
-    }
-}
