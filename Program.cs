8a00dac225d75260ee53a0c59ac60ca98715d763
topertz <k.markuci@gmail.com>
2025-06-01 08:00:36 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,78 @@
+using System.Data.SQLite;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.UseStaticFiles();
+
+app.MapGet("/", () => Results.Redirect("/index.html"));
+SQLiteConnection connection = DatabaseConnector.Db();
+SQLiteCommand command = connection.CreateCommand();
+command.CommandText = "PRAGMA foreign_keys = ON;" +
+    "CREATE TABLE IF NOT EXISTS `User` (" +
+    "`UserID` INTEGER NOT NULL PRIMARY KEY, " +
+    "`Username` TEXT NOT NULL, " +
+    "`PasswordHash` TEXT NOT NULL, " +
+    "`PasswordSalt` TEXT NOT NULL, " +
+    "`Role` TEXT NOT NULL);" +
+
+    "CREATE TABLE IF NOT EXISTS `Grade` (" +
+    "`GradeID` INTEGER NOT NULL PRIMARY KEY, " +
+    "`UserID` INTEGER NOT NULL, " +
+    "`Subject` TEXT NOT NULL, " +
+    "`GradeValue` INTEGER NOT NULL, " +
+    "`Date` TEXT NOT NULL, " +
+    "FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`));" +
+
+    "CREATE TABLE IF NOT EXISTS `Homework` (" +
+    "`HomeworkID` INTEGER NOT NULL PRIMARY KEY, " +
+    "`UserID` INTEGER NOT NULL, " +
+    "`Subject` TEXT NOT NULL, " +
+    "`Description` TEXT NOT NULL, " +
+    "`DueDate` TEXT NOT NULL, " +
+    "FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`));" +
+
+    "CREATE TABLE IF NOT EXISTS `Message` (" +
+    "`MessageID` INTEGER NOT NULL PRIMARY KEY, " +
+    "`SenderID` INTEGER NOT NULL, " +
+    "`ReceiverID` INTEGER NOT NULL, " +
+    "`MessageText` TEXT NOT NULL, " +
+    "`Timestamp` TEXT NOT NULL, " +
+    "FOREIGN KEY (`SenderID`) REFERENCES `Users` (`UserID`), " +
+    "FOREIGN KEY (`ReceiverID`) REFERENCES `Users` (`UserID`));" +
+
+    "CREATE TABLE IF NOT EXISTS `Timetable` (" +
+    "`TimetableID` INTEGER NOT NULL PRIMARY KEY, " +
+    "`Day` TEXT NOT NULL, " +
+    "`Hour` TEXT NOT NULL, " +
+    "`Subject` TEXT NOT NULL, " +
+    "`Room` TEXT NOT NULL, " +
+    "`TeacherID` INTEGER NOT NULL, " +
+    "FOREIGN KEY (`TeacherID`) REFERENCES `Users` (`UserID`));" +
+
+    "CREATE TABLE IF NOT EXISTS `Lunch` (" +
+    "`LunchID` INTEGER NOT NULL PRIMARY KEY, " +
+    "`Day` TEXT NOT NULL, " +
+    "`Meal` TEXT NOT NULL);";
+
+command.ExecuteNonQuery();
+command.Dispose();
+
+app.Run();
